6e178232b60f365afe6a65d79f18158b2165f214
bugsyflyer <bodonnell@olin.edu>
2025-10-30 11:34:06 -0400

    combined keypoint detection with centroid calculation

--- experimenting_scripts/test_keypoint_detection.py
+++ experimenting_scripts/test_keypoint_detection.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 print("Start loading image")
 # Load an image from the media folder
-raw_img = cv2.imread("media/human_follow_2.jpg")
+raw_img = cv2.imread("../media/human_follow_2.jpg")
 assert raw_img is not None, "file could not be read"
 print("Loaded image, starting algorithm")
 downscaling_factor = 4 # lower resolution so it goes faster
@@ -45,7 +45,7 @@ print("done plotting")
 print("Starting keypoint matching")
 
 # Load next image
-raw_new_img = cv2.imread("media/human_follow_3.jpg")
+raw_new_img = cv2.imread("../media/human_follow_3.jpg")
 assert raw_new_img is not None, "file could not be read"
 image_new = cv2.resize(raw_new_img, (new_width, new_height))
 
@@ -62,8 +62,33 @@ matches = bf.match(descriptors,descriptors_2)
  
 # Sort them in the order of their distance.
 matches = sorted(matches, key = lambda x:x.distance)
- 
+
 # Draw first 10 matches.
 img3 = cv2.drawMatches(image,keypoints,image_new,keypoints_2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
- 
+
 plt.imshow(img3),plt.show()
+
+#find center
+list_keypoints_2 = []
+list_kp2 = [keypoints_2[item.trainIdx].pt for item in matches[:10]]
+
+points = []
+
+for point in list_kp2:
+    points.append([point[0], point[1]])
+
+points_array = np.array(points)
+
+# median and Median Absolute Deviation
+median = np.median(points_array, axis=0)
+
+mad = np.median(np.abs(points_array - median), axis=0)
+threshold = 3.0
+
+#makes boolean array
+mask = np.all(np.abs(points_array - median) / (mad + 1e-6) < threshold, axis=1)
+
+filtered_points = points_array[mask]
+mean_point = np.mean(filtered_points, axis=0)
+
+print(mean_point)
